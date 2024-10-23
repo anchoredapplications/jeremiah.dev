82aea01fa4cab26623b5d673948e28f1df0d9f44
Jeremiah Gage <jgage6@oreillyauto.com>
2024-10-22 19:27:14 -0500

    build

--- app/api/me/skills/route.ts
+++ app/api/me/skills/route.ts
@@ -1,8 +1,7 @@
 import { getSkillsData } from '@/server/getSkillsData';
 import { NextResponse } from 'next/server'
-const { skills } = require("../../data");
 
 export async function POST(request: Request) { 
   const data = await getSkillsData()
-  return NextResponse.json(skills)
+  return NextResponse.json(data)
 }
