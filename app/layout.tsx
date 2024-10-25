ff79f9aa5b23c71d4e827e48965dd7d148d9e934
Jeremiah Gage <jgage6@oreillyauto.com>
2024-10-25 18:42:14 -0500

    Build;

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider"
-import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 const inter = Inter({ subsets: ["latin"] });
 
